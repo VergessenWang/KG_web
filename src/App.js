7ad9322dc8d8de65ffc991e1ccd27ef2846fec91
vergessenwang <469294979@qq.com>
2020-06-25 02:10:27 +0800

    nav redesign

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Introduce from './pages/Introduce'
+import Knowledge from './pages/Knowledge'
+import Robot from './pages/Robot'
+import NotFound from './pages/NotFound'
+
+import {Route,Switch} from 'react-router-dom'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path='/' component={Home}/>
+        <Route exact path='/introduce' component={Introduce} />
+        <Route exact path='/knowledge' component={Knowledge}/>
+        <Route exact path='/robot' component={Robot} />
+        <Route component={NotFound} />
+      </Switch>
+    </  > 
   );
 }
 
