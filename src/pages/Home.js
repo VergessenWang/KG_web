df3470d0a990f7220660d6dfc37d4b37b6f18a0a
vergessenwang <469294979@qq.com>
2020-06-27 02:04:52 +0800

    home_design

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './home.css'
+import {Link} from 'react-router-dom'
 
 export default function Home() {
-    return (
-        <div>
-            Home
+
+
+        return (
+        <div className="container">
+            <div className="introduce-box">
+                <h3>项目介绍</h3>
+                <p>这是一个金融知识图谱项目</p>
+                <Link to="./introduce" className="btn-primary">
+                    learn more..
+                </Link>
+            </div>
         </div>
     )
 }
